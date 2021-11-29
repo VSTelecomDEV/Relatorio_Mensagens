fd1c64534809c1a5b4dc4f5e07a1f6d254b7c97a
Leonardo Martins <80913321+VSTLeonardo@users.noreply.github.com>
2021-11-29 08:58:55 -0300

    Delete Config.php
    Security

--- app/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("BD_HOST", "127.0.0.1");
-define("BD_USERNAME", "root");
-define("BD_PASSWORD", "");
-define("BD_NAME", "twilio");
-
-define("ACCOUNT_SID", "AC760deb0ee084aae1cbe4bf08c52fd625");
-define("AUTH_TOKEN", "bd3ee20f18c9937a62bd330872a71403");
